e27bb00802331f90af8813c622ec53bbb37b2855
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2022-10-13 15:49:55 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './containers/App';
+import 'tachyons';
+// ReactDOM.render(<App/>, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App/>)
+
+
+
+
